5c16dfcf62356ba755c718c02695f3d54f525b24
Sara Arideh <sara.arzideh@gmail.com>
2023-11-17 00:28:02 +0200

    graphs added

--- frontend/src/components/DietHisroryGraph.js
+++ frontend/src/components/DietHisroryGraph.js
@@ -1,13 +1,11 @@
-import React,{ useEffect, useState, useRef } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
-function WeightHistoryGraph() {
-  const weights= useSelector(state=> state.weight.weights);
+const LineChart = ({ data }) => {
   const d3Container = useRef(null);
 
   useEffect(() => {
-    if (weights && d3Container.current) {
+    if (data && d3Container.current) {
       // Set up scales and axes
       const margin = { top: 20, right: 30, bottom: 30, left: 40 };
       const width = 400 - margin.left - margin.right;
@@ -15,12 +13,12 @@ function WeightHistoryGraph() {
 
       // Scale for X-axis
       const x = d3.scaleTime()
-        .domain(d3.extent(weights, d => new Date(d.date)))
+        .domain(d3.extent(data, d => new Date(d.date)))
         .range([0, width]);
 
       // Scale for Y-axis
       const y = d3.scaleLinear()
-        .domain([0, d3.max(weights, d => d.weight)])
+        .domain([0, d3.max(data, d => d.weight)])
         .range([height, 0]);
 
       // Line generator
@@ -47,13 +45,13 @@ function WeightHistoryGraph() {
 
       // Add the line path
       svg.append('path')
-        .datum(weights)
+        .datum(data)
         .attr('fill', 'none')
         .attr('stroke', 'steelblue')
         .attr('stroke-width', 1.5)
         .attr('d', line);
-      }
-    }, [weights, d3Container.current]);
+    }
+  }, [data, d3Container.current]);
 
   return (
     <div>
@@ -61,4 +59,5 @@ function WeightHistoryGraph() {
     </div>
   );
 };
-export default WeightHistoryGraph;
+
+export default LineChart;
