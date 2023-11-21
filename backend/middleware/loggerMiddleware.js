6f35652cd4c0d41dd5b0a98efb404a3de7bf5043
Sara Arideh <sara.arzideh@gmail.com>
2023-11-21 23:43:29 +0200

    middleware updated

--- backend/middleware/loggerMiddleware.js
+++
@@ -1,9 +0,0 @@
-// to validate request data
-//(can create specific validation function for each route by using libraries like 'express-validator')
-// for example:
-const loggerMiddleware = (req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
-    next();
-};
-
-module.exports = loggerMiddleware;
